8e3448b21eaecab10263ee427855f0af3af7b1a6
tai <taikinhmon2@gmail.com>
2021-11-27 10:57:10 +0700

    update tiep

--- app js/app.js
+++ app js/app.js
@@ -10,13 +10,16 @@ const sunrise = $('.sunrise')
 const humiditys = $('.humidity')
 const sundown = $('.sundown')
 const speeds = $('.speed')
+const tempMax = $('.max')
+const tempMin = $('.min')
 search.onchange = function(e) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${apikey}&units=metric&lang=vi`)
     .then(async res => {
         const data = await res.json()
-        console.log(data)
         namecity.innerText = data.name || defaul
-        temp.innerText = Math.floor(data.main.temp)
+        temp.innerText = Math.round(data.main.temp)
+        tempMax.innerText = Math.round(data.main.temp_max)
+        tempMin.innerText = Math.round(data.main.temp_min)
         description.innerText = data.weather[0].description
         img_weather.setAttribute('src',`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
         humiditys.innerText = data.main.humidity
